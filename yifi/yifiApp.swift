a4503f189df76d7fe6ee8797f6918ec62f37115a
Nauman Ahmad <nauman-ahmad@outlook.com>
2026-02-03 23:59:05 -0500

    Initial Commit

---
+++ yifi/yifiApp.swift
@@ -0,0 +1,32 @@
+//
+//  yifiApp.swift
+//  yifi
+//
+//  Created by Nauman Ahmad on 2/3/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct yifiApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
